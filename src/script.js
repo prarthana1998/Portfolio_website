635ea162ead726213ad37a117ea2c8a789169bfb
prarthana1998 <12prarthana.kandwal@gmail.com>
2024-04-17 15:07:35 +0100

    Adding profile section changes

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
+    var menu = document.querySelector(".menu-links");
+    var icon = document.querySelector(".hamburger-icon");
     menu.classList.toggle("open");
     icon.classList.toggle("open");
   }
