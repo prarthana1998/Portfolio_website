c30ad90097417e0d623521790a33773532c369c8
prarthana1998 <12.prarthanakandwal@gmail.com>
2024-10-22 17:41:34 +0100

    fixing changes

--- script.js
+++ script.js
@@ -4,4 +4,10 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
   }
-  
+  var typed = new Typed("#headline", {
+    strings: ["Software Engineer", "Problem-Solver"],
+    loop: true,
+    typeSpeed: 50,
+    backSpeed:50,
+    backDelay: 1500
+  });
